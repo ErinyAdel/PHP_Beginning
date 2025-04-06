b39ce54ef7d253f1790282a80674df0ed957bbde
Eriny Adel <renyadel7@gmail.com>
2025-04-06 22:22:56 +0200

    Including Files

--- Include_Require/index.php
+++ Include_Require/index.php
@@ -14,7 +14,7 @@
   <h1 class="text-center">Welcome To Home Page</h1>
 </div>
 
-<?php include 'footer.php'; ?>
+<?php require 'footer.php'; ?>
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
