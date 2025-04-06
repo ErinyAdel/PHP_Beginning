822adac00438af807ba319e6becdf9bd6f50cdb3
Eriny Adel <renyadel7@gmail.com>
2025-04-06 15:58:16 +0200

    Separate Each Topic into a Folder

--- Forms/contacts.js
+++ Forms/contacts.js
@@ -2,22 +2,22 @@
     {
         "id": 1727664286,
         "name": "Eriny Adel Badr",
-        "email": "renyadel7@gmail.com",
-        "phone": "01201414212",
+        "email": "ereny.adel@gmail.com",
+        "phone": "01201141121",
         "image": "uploads\/1743617219_pexels-quang-nguyen-vinh-222549-2131614.jpg"
     },
     {
         "id": 1727664287,
         "name": "Eriny Adel",
-        "email": "reny.adel@yahoo.com",
-        "phone": "01201414212",
+        "email": "ereny.adel@yahoo.com",
+        "phone": "01201141121",
         "image": "uploads\/istockphoto-814423752-2048x2048_222549-2231614.jpg"
     },
     {
         "id": 1727664288,
-        "name": "Eriny Adel Badr Louis",
-        "email": "renyadel7@gmail.com",
-        "phone": "01201414212",
+        "name": "NaNa",
+        "email": "nana.adel@yahoo.com",
+        "phone": "01201141121",
         "image": "uploads\/istockphoto-814423752-2048x2048_222549-2232014.jpg"
     }
 ]
